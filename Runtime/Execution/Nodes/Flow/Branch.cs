25588bcf2c30ae54a308350554ef54e8994469c0
Sqaishy <JakeEbudd@icloud.com>
2026-03-16 15:45:59 +0000

    New Branch Node
    Can now branch between true or false branches based on the result of the conditions

---
+++ Runtime/Execution/Nodes/Flow/Branch.cs
@@ -0,0 +1,109 @@
+using System;
+using UnityEngine;
+
+namespace StateTree
+{
+	/// <summary>
+	/// Chooses a branch depending on the condition result
+	/// </summary>
+	[Serializable]
+	public class Branch : Flow
+	{
+		[SerializeReference] private StateNode trueNode;
+		[SerializeReference] private StateNode falseNode;
+
+		private Condition[] conditions;
+		private ConditionOperator conditionOperator;
+		private StateNode currentChild;
+
+		public StateNode TrueNode
+		{
+			get => trueNode;
+			set => trueNode = value;
+		}
+
+		public StateNode FalseNode
+		{
+			get => falseNode;
+			set => falseNode = value;
+		}
+
+		public Branch(Condition[] conditions) : this(conditions, ConditionOperator.AllTrue)
+		{
+
+		}
+
+		public Branch(Condition[] conditions, ConditionOperator conditionOperator)
+		{
+			this.conditions = conditions;
+			this.conditionOperator = conditionOperator;
+		}
+
+		protected override Status Enter()
+		{
+			foreach (Condition condition in conditions)
+			{
+				condition.SetAgent(Module.Graph.Agent);
+				condition.Enter();
+			}
+
+			Status status;
+
+			if (Condition.CheckConditions(conditions, conditionOperator))
+			{
+				//Start the True node
+				status = Module.StartNode(trueNode);
+				currentChild = trueNode;
+			}
+			else
+			{
+				//Start the false node
+				status = Module.StartNode(falseNode);
+				currentChild = falseNode;
+			}
+
+			return status;
+		}
+
+		protected override Status Update()
+		{
+			Status status = currentChild.OnUpdate();
+
+			if (status is Status.Success or Status.Failure)
+				return status;
+
+			if (Condition.CheckConditions(conditions, conditionOperator))
+			{
+				if (currentChild != TrueNode)
+					SwitchBranch(TrueNode);
+			}
+			else
+			{
+				if (currentChild != FalseNode)
+					SwitchBranch(FalseNode);
+			}
+
+			return status;
+		}
+
+		protected override void Exit()
+		{
+			currentChild = null;
+
+			foreach (Condition condition in conditions)
+				condition.Exit();
+		}
+
+		private void SwitchBranch(StateNode newChild)
+		{
+			Module.ExitNode(currentChild);
+			currentChild = newChild;
+			Module.StartNode(currentChild);
+		}
+
+		protected override Status StartChild(Status childStatus)
+		{
+			return childStatus;
+		}
+	}
+}
